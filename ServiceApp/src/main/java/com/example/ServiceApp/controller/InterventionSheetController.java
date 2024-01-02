40c4a475e141a06658fd41b86ebb4ec3c6e8cb50
Iulian Florea <iulian.florea4@gmail.com>
2024-01-02 17:54:42 +0200

    metoda de create pentru intervention sheet

--- ServiceApp/src/main/java/com/example/ServiceApp/controller/InterventionSheetController.java
+++ ServiceApp/src/main/java/com/example/ServiceApp/controller/InterventionSheetController.java
@@ -1,4 +1,23 @@
 package com.example.ServiceApp.controller;
 
+import com.example.ServiceApp.dto.InterventionSheetDto;
+import com.example.ServiceApp.service.InterventionSheetService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/intervention-sheet")
 public class InterventionSheetController {
+
+    private final InterventionSheetService interventionSheetService;
+
+    public InterventionSheetController(InterventionSheetService interventionSheetService) {
+        this.interventionSheetService = interventionSheetService;
+    }
+@PostMapping
+    public InterventionSheetDto create(@RequestBody InterventionSheetDto interventionSheetDto) {
+        return interventionSheetService.create(interventionSheetDto);
+    }
 }
