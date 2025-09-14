e6bea13717a8f25ae7d3c3eedc548361ccaddbcf
Iulian <iulian.florea4@gmail.com>
2025-09-14 17:48:16 +0300

    m

--- ServiceApp/src/main/java/com/example/ServiceApp/config/SecurityConfiguration.java
+++ ServiceApp/src/main/java/com/example/ServiceApp/config/SecurityConfiguration.java
@@ -1,6 +1,7 @@
 package com.example.ServiceApp.config;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
