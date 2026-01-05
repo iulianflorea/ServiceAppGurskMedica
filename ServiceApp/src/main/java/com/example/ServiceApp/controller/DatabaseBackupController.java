d878a06f1eb9982816d0c042ec32588f04637d5c
Iulian Florea <iulian.florea4@gmail.com>
2026-01-05 22:46:19 +0200

    s-a rezolvat problema la backup manual

--- ServiceApp/src/main/java/com/example/ServiceApp/controller/DatabaseBackupController.java
+++ ServiceApp/src/main/java/com/example/ServiceApp/controller/DatabaseBackupController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/backup")
+@RequestMapping("/backup")
 public class DatabaseBackupController {
 
     private final DatabaseBackupTask dbBackupTask;
